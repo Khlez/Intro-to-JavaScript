1262defb1468ff792cea0159c0152f891d35691d
Khlez <98202691+Khlez@users.noreply.github.com>
2022-06-24 12:17:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/*console.log("Hello World!");
+console.log("My name: Chidinma Emeasor");
+console.log("My height: 5'2");
+console.log("Country: Nigeria"); */
+
+
+
+document.write("Hello World!<br> Name: Chidinma Emeasor<br> Height: 5'2<br> Country: Nigeria")
+ 
+alert("Name: Chidinma Emeasor\nHeight: 5'2\nCountry: Nigeria")
